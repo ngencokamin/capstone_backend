46b7bd5e88e8ec5efb37237936286759b2ba3d9e
nix <ngencokamin@gmail.com>
2021-03-31 17:15:45 -0700

    Temporarily removes password length requirement to stop db:seed from breaking

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
-  validates :password, length: { minimum: 8}, :confirmation => true, :on => :create
+  # validates :password, length: { minimum: 8}, :confirmation => true, :on => :create
   validates :bio, length: { maximum: 500 }
 
 end
