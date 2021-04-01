1c79b368aa5c51c6a1269653545f58ad422cf224
nix <ngencokamin@gmail.com>
2021-03-31 17:07:01 -0700

    Dumps data to seeds file to add to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,84 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Media.create!([
+  {omdb_id: "tt0386676", title: "The Office", imdb_rating: "8.9", released: "24 Mar 2005", plot: "A mockumentary on a group of typical office workers, where the workday consists of ego clashes, inappropriate behavior, and tedium.", poster: "https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_SX300.jpg", rated: "TV-14"},
+  {omdb_id: "tt0458290", title: "Star Wars: The Clone Wars", imdb_rating: "8.3", released: "03 Oct 2008", plot: "Jedi Knights lead the Grand Army of the Republic against the droid army of the Separatists.", poster: "https://m.media-amazon.com/images/M/MV5BZWFlNzRmOTItZjY1Ni00ZjZkLTk5MDgtOGFhOTYzNWFhYzhmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg", rated: "TV-PG"},
+  {omdb_id: "tt2930604", title: "Star Wars Rebels", imdb_rating: "8.0", released: "26 Sep 2014", plot: "A brave and clever ragtag starship crew stands up against the evil Empire as it tightens its grip on the galaxy and hunts down the last of the Jedi Knights.", poster: "https://m.media-amazon.com/images/M/MV5BY2Q1ZTAzNzMtMzlmNy00NjdjLThhYjgtMzgxN2ZkYmFhMDIwXkEyXkFqcGdeQXVyMjg5NDMwMQ@@._V1_SX300.jpg", rated: "TV-Y7-FV"},
+  {omdb_id: "tt1439629", title: "Community", imdb_rating: "8.5", released: "17 Sep 2009", plot: "A suspended lawyer is forced to enroll in a community college with an eccentric staff and student body.", poster: "https://m.media-amazon.com/images/M/MV5BNDQ5NDZiYjktZmFmMy00MjAxLTk1MDktOGZjYTY5YTE1ODdmXkEyXkFqcGdeQXVyNjcwMzEzMTU@._V1_SX300.jpg", rated: "TV-14"},
+  {omdb_id: "tt1266020", title: "Parks and Recreation", imdb_rating: "8.6", released: "09 Apr 2009", plot: "The absurd antics of an Indiana town's public officials as they pursue sundry projects to make their city a better place.", poster: "https://m.media-amazon.com/images/M/MV5BMjA5MjUxNDgwNF5BMl5BanBnXkFtZTgwMDI5NjMwNDE@._V1_SX300.jpg", rated: "TV-14"},
+  {omdb_id: "tt4955642", title: "The Good Place", imdb_rating: "8.2", released: "19 Sep 2016", plot: "Four people and their otherworldly frienemy struggle in the afterlife to define what it means to be good.", poster: "https://m.media-amazon.com/images/M/MV5BYmMxNjM0NmItNGU1Mi00OGMwLTkzMzctZmE3YjU1ZDE4NmFjXkEyXkFqcGdeQXVyODUxOTU0OTg@._V1_SX300.jpg", rated: "TV-PG"},
+  {omdb_id: "tt0149460", title: "Futurama", imdb_rating: "8.4", released: "28 Mar 1999", plot: "Philip J. Fry, a pizza delivery boy, is accidentally frozen in 1999 and thawed out on New Year's Eve 2999.", poster: "https://m.media-amazon.com/images/M/MV5BNzA2ZDk2ZTUtMWU2Yi00NDVmLTk1ODEtMmFmMjQyNWYzODI0XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg", rated: "TV-14"},
+  {omdb_id: "tt0417299", title: "Avatar: The Last Airbender", imdb_rating: "9.2", released: "21 Feb 2005", plot: "In a war-torn world of elemental magic, a young boy reawakens to undertake a dangerous mystic quest to fulfill his destiny as the Avatar, and bring peace to the world.", poster: "https://m.media-amazon.com/images/M/MV5BODc5YTBhMTItMjhkNi00ZTIxLWI0YjAtNTZmOTY0YjRlZGQ0XkEyXkFqcGdeQXVyODUwNjEzMzg@._V1_SX300.jpg", rated: "TV-Y7-FV"},
+  {omdb_id: "tt1695360", title: "The Legend of Korra", imdb_rating: "8.4", released: "14 Apr 2012", plot: "Avatar Korra fights to keep Republic City safe from the evil forces of both the physical and spiritual worlds.", poster: "https://m.media-amazon.com/images/M/MV5BNTRmM2E5OWYtYmYzMi00MDRiLTg2ZTYtZGQyZTNhZDMxYTgzXkEyXkFqcGdeQXVyODUwNjEzMzg@._V1_SX300.jpg", rated: "TV-Y7-FV"},
+  {omdb_id: "tt8688814", title: "The Dragon Prince", imdb_rating: "8.4", released: "14 Sep 2018", plot: "Two human princes forge an unlikely bond with the elfin assassin sent to kill them, embarking on an epic quest to bring peace to their warring lands.", poster: "https://m.media-amazon.com/images/M/MV5BMjA5MjEwODU1MV5BMl5BanBnXkFtZTgwNzk0MzA5NTM@._V1_SX300.jpg", rated: "TV-Y7"},
+  {omdb_id: "tt8111088", title: "The Mandalorian", imdb_rating: "8.8", released: "12 Nov 2019", plot: "The travels of a lone bounty hunter in the outer reaches of the galaxy, far from the authority of the New Republic.", poster: "https://m.media-amazon.com/images/M/MV5BZDhlMzY0ZGItZTcyNS00ZTAxLWIyMmYtZGQ2ODg5OWZiYmJkXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_SX300.jpg", rated: "TV-14"},
+  {omdb_id: "tt1561755", title: "Bob's Burgers", imdb_rating: "8.1", released: "09 Jan 2011", plot: "Bob Belcher runs his dream restaurant with his wife and three children as their last hope of holding the family together.", poster: "https://m.media-amazon.com/images/M/MV5BZGJiNmM1NDctNWUxYS00YzE4LWJjNTgtYTJhYzE0NjFmMTMwXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg", rated: "TV-PG"},
+  {omdb_id: "tt1486217", title: "Archer", imdb_rating: "8.6", released: "17 Sep 2009", plot: "Covert black ops and espionage take a back seat to zany personalities and relationships between secret agents and drones.", poster: "https://m.media-amazon.com/images/M/MV5BMTg3NTMwMzY2OF5BMl5BanBnXkFtZTgwMDcxMjQ0NDE@._V1_SX300.jpg", rated: "TV-MA"},
+  {omdb_id: "tt1538039", title: "Cats 101", imdb_rating: "8.2", released: "04 Dec 2008", plot: "A fun crash course in all things cats! With a focus on cat breeds and behaviour, Cats 101 observes the many different features of your feline friend.", poster: "https://m.media-amazon.com/images/M/MV5BMjI2NDA2NDYyNF5BMl5BanBnXkFtZTcwMDcxMzExNQ@@._V1_SX300.jpg", rated: "TV-PG"},
+  {omdb_id: "tt1734135", title: "Trollhunters: Tales of Arcadia", imdb_rating: "8.5", released: "23 Dec 2016", plot: "An ordinary boy finds a magic amulet that chooses him to be a Trollhunter, a guardian that defends the world of humans and the secret world of trolls.", poster: "https://m.media-amazon.com/images/M/MV5BN2Y2YWE0ZDMtNzQzMC00MDI0LThlZjAtMWY4YjZlYWM2OWFjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg", rated: "TV-Y7"},
+  {omdb_id: "tt1826940", title: "New Girl", imdb_rating: "7.7", released: "20 Sep 2011", plot: "After a bad break-up, Jess, an offbeat young woman, moves into an apartment loft with three single men. Although they find her behavior very unusual, the men support her - most of the time.", poster: "https://m.media-amazon.com/images/M/MV5BMjA0MDc1NTk0Ml5BMl5BanBnXkFtZTgwMTk2ODA5NDM@._V1_SX300.jpg", rated: "TV-14"}
+])
 
-# User.create(username: "TVGod420", email: "tvgod420@email.com", password: "password", profile_picture: "https://static.turbosquid.com/Preview/2019/02/18__04_59_25/Crash_Test_Dummy_Rigged_mb_00.jpg5B1ADE98-2892-494F-9EB2-F0B49A1BE375Default.jpg", bio: "I am the god of tv B)")
-# User.create(username: "xXxdark_lordxXx", email: "edgy@email.com", password: "password", profile_picture: "https://static.turbosquid.com/Preview/2019/02/18__04_59_25/Crash_Test_Dummy_Rigged_mb_00.jpg5B1ADE98-2892-494F-9EB2-F0B49A1BE375Default.jpg", bio: "Find me on deviantart!")
-# User.create(username: "username", email: "test@email.com", password: "password", profile_picture: "https://static.turbosquid.com/Preview/2019/02/18__04_59_25/Crash_Test_Dummy_Rigged_mb_00.jpg5B1ADE98-2892-494F-9EB2-F0B49A1BE375Default.jpg", bio: "I am a real user :)))))")
+User.create!([
+  {username: "$nix", email: "ngencokamin@gmail.com", password_digest: "$2a$12$qOj2o/00MFoVJF0bhkh5w.pU2CkdmR2DF7TomNFqh7nfFDiDHWn0.", profile_picture: "https://res.cloudinary.com/drdocnfbj/image/upload/v1616654776/nljhqs3xjjcyeyf7w8ek.jpg", bio: "My name's Nix, I'm a Full-Stack Developer. I hope you like my site :)", favorite_media_id: 2, profanity_filter: false, trello_list_id: "605e129c0738a68e1020cbcf"},
+  {username: "catlover", email: "catlover@email.com", password_digest: "$2a$12$0ksWOpWtIBpZBouZRuXgDeTQBv/D2Gm8bZeufdQMj.nbs8rwuBRlC", profile_picture: "https://res.cloudinary.com/drdocnfbj/image/upload/v1616650910/e1shlrnwrxmgn3ovof6q.jpg", bio: "Hi there!\nIn my mind, there's nothing better than relaxing and watching TV with a cat on my lap after a long day at work. All shows I suggest are cat approved!", favorite_media_id: 15, profanity_filter: false, trello_list_id: nil},
+  {username: "darthbinks", email: "jarjar@email.com", password_digest: "$2a$12$ggeZ9vtXashZycyeEKZ2f.epZhsTUCqCoDLM75CntCSdlt/ISjbLe", profile_picture: "https://res.cloudinary.com/drdocnfbj/image/upload/v1616652606/j82t2wrlilszhtgyfz2e.jpg", bio: "JAR JAR BINKS IS A SITH LORD! WAKE UP SHEEPLE! SHEEV WAS A PUPPET!", favorite_media_id: 2, profanity_filter: false, trello_list_id: nil},
+  {username: "hypnotoad", email: "hypnotoad@email.com", password_digest: "$2a$12$liFH4Ezccu3tPDrlHrSZYeI6RbodESEmVI7qPGzXR0q5OyyELO9Ky", profile_picture: "https://res.cloudinary.com/drdocnfbj/image/upload/v1616653489/j8imf6ul6fxivudxemss.jpg", bio: "All glory to the hypnotoad!", favorite_media_id: 7, profanity_filter: false, trello_list_id: nil}
+])
+SavedShow.create!([
+  {user_id: 1, media_id: 11, trello_id: nil},
+  {user_id: 1, media_id: 13, trello_id: nil},
+  {user_id: 1, media_id: 7, trello_id: nil},
+  {user_id: 1, media_id: 12, trello_id: nil},
+  {user_id: 1, media_id: 3, trello_id: nil},
+  {user_id: 1, media_id: 10, trello_id: nil},
+  {user_id: 2, media_id: 1, trello_id: nil},
+  {user_id: 2, media_id: 8, trello_id: nil},
+  {user_id: 2, media_id: 13, trello_id: nil},
+  {user_id: 2, media_id: 7, trello_id: nil},
+  {user_id: 2, media_id: 9, trello_id: nil},
+  {user_id: 2, media_id: 5, trello_id: nil},
+  {user_id: 3, media_id: 7, trello_id: nil},
+  {user_id: 4, media_id: 13, trello_id: "605e129e59f4231b4fa54043"},
+  {user_id: 4, media_id: 8, trello_id: "605e129e4a452f3a92c6f366"},
+  {user_id: 4, media_id: 10, trello_id: "605e129e5bb74d2a675a7d5e"},
+  {user_id: 4, media_id: 15, trello_id: "605e129e6f3fcc529635be64"},
+  {user_id: 4, media_id: 22, trello_id: "605e129edc89fd1b8e554da8"}
+])
+Comment.create!([
+  {text: "Parks and Rec conjures up familiar feelings with its mockumentary brand humor centering around a workplace, but differs enough to keep from feeling stale. Highly suggest this show (but maybe skip the first season)", similarity: 6, enjoyability: 8, media_id: 1, user_id: 1, suggested_media_id: 5},
+  {text: "While The Good Place may lack much of Community's trademark meta humor, they're both hilarious and heartwarming tales about chosen family trying to become better people.", similarity: 4, enjoyability: 9, media_id: 4, user_id: 1, suggested_media_id: 6},
+  {text: "The Legend of Korra is a pretty good sequel to Avatar. It's not as good, but it's still a decent watch if you want more of the Avatar universe", similarity: 8, enjoyability: 7, media_id: 8, user_id: 1, suggested_media_id: 9},
+  {text: "Rebels is really good. It's more childish at first but it's still really good guys. Filoni is saving Star Wars, even Rebels.", similarity: 6, enjoyability: 7, media_id: 2, user_id: 2, suggested_media_id: 3},
+  {text: "If you haven't seen TCW, you owe it to yourself. Filoni is a genius, and TCW is definitely not a kid's show.", similarity: 8, enjoyability: 10, media_id: 11, user_id: 2, suggested_media_id: 2},
+  {text: "Fans of Avatar would probably like Rebels. It still covers heavy subjects like Avatar, but without the brutal violence of The Clone Wars.", similarity: 4, enjoyability: 8, media_id: 8, user_id: 2, suggested_media_id: 3},
+  {text: "HYPNOTOAD SAYS WATCH THE DRAGON PRINCE", similarity: 6, enjoyability: 8, media_id: 8, user_id: 3, suggested_media_id: 10},
+  {text: "HYPNOTOAD SAYS WATCH FUTURAMA", similarity: 5, enjoyability: 10, media_id: 12, user_id: 3, suggested_media_id: 7},
+  {text: "HYPNOTOAD SAYS WATCH ARCHER", similarity: 6, enjoyability: 8, media_id: 7, user_id: 3, suggested_media_id: 13},
+  {text: "HYPNOTOAD SAYS WATCH COMMUNITY", similarity: 5, enjoyability: 9, media_id: 1, user_id: 3, suggested_media_id: 4},
+  {text: "Trollhunters is honestly fantastic. Highly suggest giving it a watch, you won't regret it!", similarity: 7, enjoyability: 9, media_id: 8, user_id: 4, suggested_media_id: 16},
+  {text: "Both heartfelt comedies about chosen family, definitely give The Good Place a shot.", similarity: 6, enjoyability: 9, media_id: 22, user_id: 4, suggested_media_id: 6}
+])
 
-# Media.create(omdb_id: "tt0417299", title: "Avatar: The Last Airbender", imdb_rating: 9.2, released: "21 Feb 2005", plot: "In a war-torn world of elemental magic, a young boy reawakens to undertake a dangerous mystic quest to fulfill his destiny as the Avatar, and bring peace to the world", poster: "https://m.media-amazon.com/images/M/MV5BODc5YTBhMTItMjhkNi00ZTIxLWI0YjAtNTZmOTY0YjRlZGQ0XkEyXkFqcGdeQXVyODUwNjEzMzg@._V1_SX300.jpg")
-# Media.create(omdb_id: "tt0386676", title: "The Office", imdb_rating: 8.9, released: "24 Mar 2005", plot: "A mockumentary on a group of typical office workers, where the workday consists of ego clashes, inappropriate behavior, and tedium.", poster: "https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_SX300.jpg")
-# Media.create(omdb_id: "tt1695360", title: "The Legend of Korra", imdb_rating: 8.4, released: "14 Apr 2012", plot: "Avatar Korra fights to keep Republic City safe from the evil forces of both the physical and spiritual worlds.", poster: "https://m.media-amazon.com/images/M/MV5BNTRmM2E5OWYtYmYzMi00MDRiLTg2ZTYtZGQyZTNhZDMxYTgzXkEyXkFqcGdeQXVyODUwNjEzMzg@._V1_SX300.jpg")
-# Media.create(omdb_id: "tt8688814", title: "The Dragon Prince", imdb_rating: 8.4, released: "14 Sep 2018", plot: "Two human princes forge an unlikely bond with the elfin assassin sent to kill them, embarking on an epic quest to bring peace to their warring lands.", poster: "https://m.media-amazon.com/images/M/MV5BMjA5MjEwODU1MV5BMl5BanBnXkFtZTgwNzk0MzA5NTM@._V1_SX300.jpg")
-# Media.create(omdb_id: "tt1439629", title: "Community", imdb_rating: 8.5, released: "17 Sep 2009", plot: "A suspended lawyer is forced to enroll in a community college with an eccentric staff and student body.", poster: "https://m.media-amazon.com/images/M/MV5BNDQ5NDZiYjktZmFmMy00MjAxLTk1MDktOGZjYTY5YTE1ODdmXkEyXkFqcGdeQXVyNjcwMzEzMTU@._V1_SX300.jpg")
-
-# Comment.create(text: "If you want a similar feel to Avatar, look no further than legend of korra! Though for many it falls flat, it's a very competant continuation.", similarity: 8, enjoyability: 7, media_id: 1, user_id: 1, suggested_media_id: 3)
-# Comment.create(text: "Though it definitely differs in tone and feel, the dragon prince is a must watch that most Avatar fans will enjoy.", similarity: 7, enjoyability: 9, media_id: 1, user_id: 2, suggested_media_id: 4)
-# Comment.create(text: "Though quite different, community is better than the office.", similarity: 4, enjoyability: 10, media_id: 2, user_id: 3, suggested_media_id: 5)
-
-# Vote.create(user_id: 2, comment_id: 1, value: -1)
-# Vote.create(user_id: 1, comment_id: 2, value: 1)
-# Vote.create(user_id: 1, comment_id: 3, value: 1)
-# Vote.create(user_id: 2, comment_id: 3, value: 1)
+Vote.create!([
+  {user_id: 1, comment_id: 1, value: 1},
+  {user_id: 2, comment_id: 5, value: 1},
+  {user_id: 2, comment_id: 1, value: 1},
+  {user_id: 2, comment_id: 2, value: 1},
+  {user_id: 1, comment_id: 2, value: 1},
+  {user_id: 2, comment_id: 3, value: 1},
+  {user_id: 3, comment_id: 8, value: 1},
+  {user_id: 3, comment_id: 3, value: 1},
+  {user_id: 3, comment_id: 10, value: 1},
+  {user_id: 3, comment_id: 11, value: 1},
+  {user_id: 3, comment_id: 1, value: 1},
+  {user_id: 4, comment_id: 9, value: 1},
+  {user_id: 1, comment_id: 12, value: 1},
+  {user_id: 2, comment_id: 12, value: 1},
+  {user_id: 1, comment_id: 3, value: -1},
+  {user_id: 4, comment_id: 13, value: -1},
+  {user_id: 4, comment_id: 12, value: -1},
+  {user_id: 4, comment_id: 8, value: 1},
+  {user_id: 4, comment_id: 14, value: 1},
+  {user_id: 4, comment_id: 15, value: 1},
+  {user_id: 4, comment_id: 16, value: -1},
+  {user_id: 4, comment_id: 17, value: -1}
+])
